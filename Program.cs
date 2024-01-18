1712cdc8b7aa1290619f41480573b1a14774cc1b
emanuelrivas243 <emanuel.rivas@correounivalle.edu.co>
2024-01-18 16:32:54 -0500

    Practice with arrays

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!");
+
+using System;
+
+namespace CSharpHelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            // 
+            Console.WriteLine("Hello C#");
+            string myString = "Silent Hill";
+            Console.WriteLine(myString);
+            
+            int myInt = 243;
+            var myArray = new string[] {"Silent Hill 2", "Zelda Twilight Princess", "Silent Hill 3"};
+            foreach (string games in myArray) 
+            {
+                Console.WriteLine($"Game: {games}");
+            }
+
+        }
+    }
+}
+
